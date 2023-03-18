83df0b7ef26ffc740756112b9195e5b709bae1ad
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-03-18 14:00:53 +0100

    some changes

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const list= [
     {
         id:1,
         title:"hello",
-        body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda"
+        body:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda",
+        pix:"./images/picx2.png"
     },
     {
         id:2,
         title:"dear",
-        body:"2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda maiores, voluptates voluptas impedit atque laboriosam incidunt doloremque fuga!"
+        body:"2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda maiores, voluptates voluptas impedit atque laboriosam incidunt doloremque fuga!",
+        pix:"./images/picx.png"
     },
     {
         id:3,
         title:"how",
-        body:"3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda maiores, voluptates "
+        body:"3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda maiores, voluptates ",
+        pix:"./images/picx2.png"
     },
     {
         id:4,
         title:"are",
-        body:"4Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda maiores, voluptates voluptas impedit atque laboriosam incidunt doloremque fuga!"
+        body:"4Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nemo assumenda maiores, voluptates voluptas impedit atque laboriosam incidunt doloremque fuga!",
+        pix:"./images/picx.png"
     },
 ]
 
@@ -26,7 +30,10 @@ function getBody(id){
 
     list.map(eachpill=>{
         if (eachpill.id === id){
-            return bodyPill.innerHTML = `<div class="pill-body">${eachpill.body}</div>`
+            return bodyPill.innerHTML = `<div class="pill-body">${eachpill.body}</div>
+            <div class="pic-box">
+            <img src="${eachpill.pix}"/>
+            </div>`
         }
     })
 }
